2dab53e88e85c2db6253d86d75925cfc6c147172
Hazal <HazalCiplak@users.noreply.github.com>
2024-12-17 11:35:08 +0000

    Simple API endpoint (#6)
    * added simple router
    
    * included basic router to the main

--- spacy_keyword_extraction_api/main.py
+++ spacy_keyword_extraction_api/main.py
@@ -3,6 +3,8 @@ import logging
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
+from spacy_keyword_extraction_api.api_router import create_api_router
+
 
 LOGGER = logging.getLogger(__name__)
 
@@ -10,7 +12,8 @@ LOGGER = logging.getLogger(__name__)
 def create_app():
     app = FastAPI()
 
-    app = FastAPI(docs_url=None, redoc_url=None)
+    app.include_router(create_api_router())
+
     app.mount('/', StaticFiles(directory='static', html=True), name='static')
 
     return app
