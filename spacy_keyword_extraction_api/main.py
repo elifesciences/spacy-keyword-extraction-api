f28e0817aa1c365207195e3df6aff6faa570b217
Daniel Ecer <de-code@users.noreply.github.com>
2024-12-18 17:15:41 +0000

    Use SpacyKeywordExtractor

--- spacy_keyword_extraction_api/main.py
+++ spacy_keyword_extraction_api/main.py
@@ -3,8 +3,11 @@ import logging
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
+import spacy
+
 from spacy_keyword_extraction_api.api_router import create_api_router
-from spacy_keyword_extraction_api.extract_keywords import SimpleKeywordExtractor
+from spacy_keyword_extraction_api.extract_keywords import SpacyKeywordExtractor
+from spacy_keyword_extraction_api.spacy_keyword import DEFAULT_SPACY_LANGUAGE_MODEL_NAME
 
 
 LOGGER = logging.getLogger(__name__)
@@ -13,7 +16,13 @@ LOGGER = logging.getLogger(__name__)
 def create_app():
     app = FastAPI()
 
-    app.include_router(create_api_router(keyword_extractor=SimpleKeywordExtractor()))
+    app.include_router(create_api_router(
+        keyword_extractor=SpacyKeywordExtractor(
+            language=spacy.load(
+                DEFAULT_SPACY_LANGUAGE_MODEL_NAME
+            )
+        )
+    ))
 
     app.mount('/', StaticFiles(directory='static', html=True), name='static')
 
