060811834eb0a79905f689dc5686b486175d7be2
HazalCiplak <hazalciplak@gmail.com>
2024-12-18 13:37:33 +0000

    extracted keyword extractor

--- spacy_keyword_extraction_api/api_router.py
+++ spacy_keyword_extraction_api/api_router.py
@@ -1,16 +1,19 @@
 import logging
+from typing import Sequence
 
 from fastapi import APIRouter
+
+from spacy_keyword_extraction_api.extract_keywords import KeywordExtractor
 
 
 LOGGER = logging.getLogger(__name__)
 
 
-def create_api_router() -> APIRouter:
+def create_api_router(keyword_extractor: KeywordExtractor) -> APIRouter:
     router = APIRouter()
 
     @router.get("/v1/extract-keywords")
-    def extract_keywords(text: str):
-        return text.split()
+    def extract_keywords(text: str) -> Sequence[str]:
+        return list(keyword_extractor.iter_extract_keywords(text_list=[text]))[0]
 
     return router
