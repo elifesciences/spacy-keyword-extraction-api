375ec0e12be4bd2ea44821e43f215583467d8aed
Daniel Ecer <de-code@users.noreply.github.com>
2024-12-27 11:41:34 +0000

    Extracted sample text

--- tests/unit_test/api_router_test.py
+++ tests/unit_test/api_router_test.py
@@ -6,6 +6,9 @@ from spacy_keyword_extraction_api.api_router import create_api_router
 from spacy_keyword_extraction_api.extract_keywords import KeywordExtractor, SimpleKeywordExtractor
 
 
+TEXT_1 = 'Some Text'
+
+
 @pytest.fixture(name='keyword_extractor')
 def _keyword_extractor():
     return SimpleKeywordExtractor()
@@ -24,7 +27,7 @@ class TestExtractKeyword:
         keyword_extractor: KeywordExtractor
     ):
         client = create_test_client(keyword_extractor=keyword_extractor)
-        response = client.get('/v1/extract-keywords', params={'text': 'Some Text'})
+        response = client.get('/v1/extract-keywords', params={'text': TEXT_1})
         assert response.json() == {
-            'keywords': list(keyword_extractor.iter_extract_keywords(text_list=['Some Text']))[0]
+            'keywords': list(keyword_extractor.iter_extract_keywords(text_list=[TEXT_1]))[0]
         }
