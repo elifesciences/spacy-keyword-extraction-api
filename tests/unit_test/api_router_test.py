9b2838cd7f56b57e59625d7cd34d45d2bd0722c3
Daniel Ecer <de-code@users.noreply.github.com>
2024-12-27 11:40:06 +0000

    Use fixture for keyword extractor

--- tests/unit_test/api_router_test.py
+++ tests/unit_test/api_router_test.py
@@ -1,21 +1,30 @@
 from fastapi.testclient import TestClient
 from fastapi import FastAPI
+import pytest
 
 from spacy_keyword_extraction_api.api_router import create_api_router
-from spacy_keyword_extraction_api.extract_keywords import SimpleKeywordExtractor
+from spacy_keyword_extraction_api.extract_keywords import KeywordExtractor, SimpleKeywordExtractor
 
 
-def create_test_client():
+@pytest.fixture(name='keyword_extractor')
+def _keyword_extractor():
+    return SimpleKeywordExtractor()
+
+
+def create_test_client(keyword_extractor: KeywordExtractor):
     app = FastAPI()
-    app.include_router(create_api_router(keyword_extractor=SimpleKeywordExtractor()))
+    app.include_router(create_api_router(keyword_extractor=keyword_extractor))
     client = TestClient(app)
     return client
 
 
 class TestExtractKeyword:
-    def test_should_return_keywords_in_lower_case(self):
-        client = create_test_client()
+    def test_should_return_keywords_in_lower_case(
+        self,
+        keyword_extractor: KeywordExtractor
+    ):
+        client = create_test_client(keyword_extractor=keyword_extractor)
         response = client.get('/v1/extract-keywords', params={'text': 'Some Text'})
         assert response.json() == {
-            'keywords': ['some', 'text']
+            'keywords': list(keyword_extractor.iter_extract_keywords(text_list=['Some Text']))[0]
         }
