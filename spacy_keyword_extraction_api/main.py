f35932786a1b0dbe3a7f694b3e5437e76e386001
Daniel Ecer <de-code@users.noreply.github.com>
2025-01-08 13:51:35 +0000

    Include commit in response metadata (#20)
    * Extracted get_app_meta
    
    * Added TestGetAppMeta
    
    * Include revision in response meta
    
    * Add VCS_REF to Docker image
    
    * Pass GITHUB_SHA as VCS_REF to GitHub Actions build

--- spacy_keyword_extraction_api/main.py
+++ spacy_keyword_extraction_api/main.py
@@ -1,5 +1,7 @@
 import logging
+import os
 import platform
+from typing import Optional
 
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
@@ -7,6 +9,7 @@ from fastapi.staticfiles import StaticFiles
 import spacy
 
 from spacy_keyword_extraction_api.api_router import create_api_router
+from spacy_keyword_extraction_api.api_router_typing import KeywordsResponseMetaTypedDict
 from spacy_keyword_extraction_api.extract_keywords import SpacyKeywordExtractor
 from spacy_keyword_extraction_api.spacy_keyword import (
     DEFAULT_SPACY_LANGUAGE_MODEL_NAME,
@@ -15,6 +18,28 @@ from spacy_keyword_extraction_api.spacy_keyword import (
 
 
 LOGGER = logging.getLogger(__name__)
+
+
+class EnvironmentVariables:
+    VCS_REF = 'VCS_REF'
+
+
+def get_vcs_ref() -> Optional[str]:
+    return os.getenv(EnvironmentVariables.VCS_REF)
+
+
+def get_app_meta(
+    spacy_language_model_name: str
+) -> KeywordsResponseMetaTypedDict:
+    meta: KeywordsResponseMetaTypedDict = {
+        'spacy_version': spacy.__version__,
+        'spacy_language_model_name': spacy_language_model_name,
+        'python_version': platform.python_version()
+    }
+    vcf_ref = get_vcs_ref()
+    if vcf_ref:
+        meta['revision'] = vcf_ref
+    return meta
 
 
 def create_app():
@@ -26,11 +51,9 @@ def create_app():
         keyword_extractor=SpacyKeywordExtractor(
             language=load_spacy_model(spacy_language_model_name)
         ),
-        meta={
-            'spacy_version': spacy.__version__,
-            'spacy_language_model_name': spacy_language_model_name,
-            'python_version': platform.python_version()
-        }
+        meta=get_app_meta(
+            spacy_language_model_name=spacy_language_model_name
+        )
     ))
 
     app.mount('/', StaticFiles(directory='static', html=True), name='static')
